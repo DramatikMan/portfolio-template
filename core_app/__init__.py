d0b1a5714b509c3c643c89c5e028c2a1bb6ae518
Sergey Pavlov <self@sergeypavlov.dev>
2021-04-24 19:17:30 +0300

    Refactor config usage

--- core_app/__init__.py
+++ core_app/__init__.py
@@ -6,8 +6,8 @@ from flask import Flask, render_template
 def create_app():
     app = Flask(__name__)
 
-    cfg = os.environ.get('FLASK_ENV').capitalize() + 'Config'
-    app.config.from_object('config.' + cfg)
+    cfg = f"config.{os.environ.get('FLASK_ENV').capitalize()}Config"
+    app.config.from_object(cfg)
     app.url_map.strict_slashes = False
 
     @app.route('/')
