a88917e283d332de93428b05e07860fde7be890c
Sergey Pavlov <self@sergeypavlov.dev>
2021-04-25 23:29:01 +0300

    Move app factory to a separate file

---
+++ core_app/factory.py
@@ -0,0 +1,22 @@
+import os
+
+from flask import Flask, render_template
+
+from core_app import celery
+from .celery_utils import init_celery
+
+
+def create_app():
+    app = Flask('core_app')
+
+    cfg = f"config.{os.environ.get('FLASK_ENV').capitalize()}Config"
+    app.config.from_object(cfg)
+    app.url_map.strict_slashes = False
+
+    init_celery(celery, app)
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    return app
