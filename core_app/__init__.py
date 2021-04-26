7e0e05b46f70cc87cee6381437a4c9692e39ac82
Sergey Pavlov <self@sergeypavlov.dev>
2021-04-26 23:51:16 +0300

    Add email sending task

--- core_app/__init__.py
+++ core_app/__init__.py
@@ -1,4 +1,6 @@
 import os
+import smtplib
+from email.message import EmailMessage
 
 from celery import Celery
 
@@ -18,3 +20,16 @@ celery = make_celery()
 @celery.task
 def log(msg):
     return msg
+
+
+@celery.task
+def send_email(email, text):
+    msg = EmailMessage()
+    msg.set_content(text)
+    msg['Subject'] = 'Contact form message'
+    msg['From'] = email
+    msg['To'] = 'self@sergeypavlov.dev'
+
+    s = smtplib.SMTP('127.0.0.1', 8025)
+    s.send_message(msg)
+    s.quit()
