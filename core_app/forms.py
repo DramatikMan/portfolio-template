2579298ce2e9dff9d08faf0d78a56a63cab427ec
Sergey Pavlov <self@sergeypavlov.dev>
2021-04-29 20:52:40 +0300

    Update form fields' labels

--- core_app/forms.py
+++ core_app/forms.py
@@ -4,5 +4,5 @@ from wtforms.validators import DataRequired, Email, Length
 
 
 class MessageForm(FlaskForm):
-    address = StringField('Your email', [DataRequired(), Email()])
-    text = TextAreaField('Your message', [DataRequired(), Length(min=10)])
+    address = StringField('Your email address:', [DataRequired(), Email()])
+    text = TextAreaField('Your message:', [DataRequired(), Length(min=10)])
